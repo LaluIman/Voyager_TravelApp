d2ffc6d170c28d4aa559a1480e53401eb3397272
Eye-Mans <laluiman468@gmail.com>
2024-12-11 14:24:23 +0700

    Big update, add a device preview, make flutter build web to make it run in web, and adding gitattributes config for github tech detection

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,23 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:travel_app/screens/welcome_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(DevicePreview(
+    enabled: true,
+    builder: (context) =>  const MyApp()
+  ));
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      builder: DevicePreview.appBuilder,
       debugShowCheckedModeBanner: false,
+      locale: DevicePreview.locale(context),
       theme: ThemeData(
         fontFamily: "Poppins"
       ),
